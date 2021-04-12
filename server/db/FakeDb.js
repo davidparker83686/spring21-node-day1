157503e74e3f0c70901ffd7a56c10675b53abbb2
davidparker83686 <davidjparker91@gmail.com>
2021-04-12 15:40:57 -0600

    done done

--- server/db/FakeDb.js
+++ server/db/FakeDb.js
@@ -1,7 +1,12 @@
 import { generateId } from "../utils/GenerateId"
 
-class FakeDb {
-  spells = [{ name: 'Ray of Fraught', description: "Its mildly worrying", id: generateId() }, { name: 'Hexblades Purse', description: "the best weapon is the old lady purse", id: generateId() }]
-}
 
+// class FakeDb {
+//   spells = [{ name: 'Ray of Fraught', description: "Its mildly worrying", id: generateId() }, { name: 'Hexblades Purse', description: "the best weapon is the old lady purse", id: generateId() }]
+// }
+
+
+class FakeDb {
+  burgers = [{ name: 'In-n-out burger', price: 5, ingredients: 'grilled onion, cheese, special sauce', id: generateId() }, { name: 'Hexblades Purse', description: "the best weapon is the old lady purse", id: generateId() }]
+}
 export const fakeDb = new FakeDb()
